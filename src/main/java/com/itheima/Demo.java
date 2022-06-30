ac0146ab9ceb1af01bb4314ba96f554e197c4529
autumn3306 <108450202+autumn3306@users.noreply.github.com>
2022-06-30 19:15:33 +0800

    Update Demo.java
    435如4

--- src/main/java/com/itheima/Demo.java
+++ src/main/java/com/itheima/Demo.java
@@ -5,6 +5,6 @@ public class Demo {
         System.out.println(123);
         System.out.println("master");
         System.out.println("hotfix");
-        System.out.println("push");
+        System.out.println("pull");
     }
 }
